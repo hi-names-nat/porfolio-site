2f96fe0711e68441600b8425e58b858ee502c87c
Natalie Soltis <natalie.soltis@outlook.com>
2024-01-22 15:22:25 -0500

    begin on update to reconstruct page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import './App.css';
-import Topbar from "./Components/Topbar/Topbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Router from "./Components/Router";
-import Footer from "./Components/Footer/Footer";
+import './output.css';
+import {faGear} from "@fortawesome/free-solid-svg-icons";
 
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function App(): JSX.Element {
 
   return (
-    <div className="App">
+    <div className={"App"}>
+      <div className={"bg-image"}>
+        <div className={"rc-header"}>
+          <h1>Natalie Soltis's Website</h1>
+          <h3>software developer with skills in both high level and low level software development</h3>
+        </div>
 
+        <div className={"slab-body"}>
+          <div className={"gear-container"}>
+            <FontAwesomeIcon className={"gear-icon"} icon={faGear} spin/>
+          </div>
+          <div className={"rc-body"}>
+            <h2>This website is currently undergoing major reconstruction.</h2>
+            <h3>Apologies for the inconvenience. Below are quick links to contact and pertinent information.</h3>
+          </div>
+          <div className={"rc-links"}>
+            <a className={"rc-grid-element"} href={"here"}>Link 1</a>
+            <a className={"rc-grid-element"} href={"here"}>Link 1</a>
+            <a className={"rc-grid-element"} href={"here"}>Link 1</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
